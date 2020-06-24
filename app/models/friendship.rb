186def89dc09a56cece284f709d0408f4f9a1f7b
hillarioh <okerioh@gmail.com>
2020-06-24 05:16:51 +0300

    add scope to friendship and its logic

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ApplicationRecord
     belongs_to :invitor, class_name: 'User'
     belongs_to :invitee, class_name: 'User'
+
+    scope :occurence_in_friendship, ->(r,e){where(invitor: r,invitee: e)} 
 end
