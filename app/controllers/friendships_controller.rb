40bbe07a7f001b0ccd3cfc49f245f12f21ccfd2c
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-24 18:33:46 +0600

    Add routes and create method

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,3 +1,11 @@
 class FriendshipsController < ApplicationController
+    def create
+        @friendship = Friendship.new(invitor_id: current_user.id, invitee_id: user.id)
     
+        if @friendship.save
+          redirect_to users_path, notice: 'You invited to be a friend.'
+        else
+          redirect_to users_path, alert: 'You cannot invite the user.'
+        end
+      end
 end
