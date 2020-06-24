a8529a0374390a4a4eb12cc37b01613c7e88a4a9
hillarioh <okerioh@gmail.com>
2020-06-24 06:02:20 +0300

    add friendship test cases

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,5 +2,8 @@ class Friendship < ApplicationRecord
     belongs_to :invitor, class_name: 'User'
     belongs_to :invitee, class_name: 'User'
 
+    validates_presence_of :invitor
+    validates_presence_of :invitee
+
     scope :occurence_in_friendship, ->(r,e){where(invitor: r,invitee: e)} 
 end
---
+++ spec/models/friendship_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Friendship, type: :model do
+    let(:friend) { Friendship.create(invitor_id: 1,invitee_id: 2) }
+  context 'Check for validation of Friendship input' do
+    it 'is valid with valid attributes' do
+      expect(Friendship.create).to_not be_valid
+    end
+
+    it 'is not valid without an invitor id' do
+      expect(Friendship.new(invitor: nil)).to_not be_valid
+    end
+
+    it 'is not valid without an invitor id' do
+       expect(Friendship.new(invitee: nil)).to_not be_valid
+    end
+
+    it 'is  valid without a status of friendship' do
+        expect(friend.status).to eq false
+    end
+  end
+
+end
