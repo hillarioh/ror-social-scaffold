4b7bd4577556c3ce67bedfc41a88844be28c9db5
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-24 18:01:43 +0600

    Add friendships controller

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -9,9 +9,9 @@ module UserHelper
         elsif Friendship.occurence_in_friendship2(id,current_user.id).count == 1
             html += 'Already Friends'
         elsif Friendship.occurence_in_friendship(current_user.id,id).count > 0
-            html += link_to('Awaiting acceptance from user', users_path)
+            html += 'Awaiting acceptance from user'
         else
-            html += link_to('Invite ')
+            html += link_to('Invite')
         end
         html.html_safe
     end
