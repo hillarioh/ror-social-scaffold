090649b8c541c10a8ccca91fbc4ab3f347cfd6dc
hillarioh <okerioh@gmail.com>
2020-06-24 09:46:47 +0300

    add reject button

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,9 +4,10 @@ module UserHelper
         html = ''
         
         if Friendship.occurence_in_friendship(id,current_user.id).count > 0
-            html += link_to('Accept frendship', users_path)
+            html += link_to('Accept friendship', users_path)
+            html += link_to('Reject friendship', users_path)
         elsif Friendship.occurence_in_friendship(current_user.id,id).count == 0
-            html += link_to('Invite to frendship', users_path)
+            html += link_to('Invite to friendship', users_path)
         end
         html.html_safe
     end
