67bf5aeeb65e550bb4626a761b9519630077a743
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-25 20:58:11 +0600

    Change link to buttons and friendship ids

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,11 +4,10 @@ module UserHelper
 
     if Friendship.occurence_in_friendship(id, current_user.id).count.positive?
       friend = Friendship.occurence_in_friendship(id, current_user.id).pluck(:id)
-      accept_params = { id: friend[0], invitor_id: current_user.id, invitee_id: id, status: true }
-      html += link_to('Accept Friendship', friendship_path(accept_params), method: :put)
-      html += ' '
-      reject_params = { id: friend[0], invitor_id: current_user.id, invitee_id: id, status: true }
-      html += link_to('Reject friendship', friendship_path(reject_params), method: :delete)
+      accept_params = { id: friend[0], user_id: current_user.id, friend_id: id, status: true }
+      html += button_to('Accept Friendship', friendship_path(accept_params), method: :put)      
+      reject_params = { id: friend[0], user_id: current_user.id, friend_id: id, status: true }
+      html += button_to('Reject friendship', friendship_path(reject_params), method: :delete)
     elsif Friendship.occurence_in_friendship2(id, current_user.id).count == 1
       html += 'Already Friends'
     elsif Friendship.occurence_in_friendship2(current_user.id, id).count == 1
@@ -16,7 +15,7 @@ module UserHelper
     elsif Friendship.occurence_in_friendship(current_user.id, id).count.positive?
       html += 'Awaiting acceptance from user'
     else
-      html += link_to('Invite', friendships_path(invitor_id: current_user.id, invitee_id: id), method: :post)
+      html += button_to('Invite', friendships_path(user_id: current_user.id, friend_id: id), method: :post)
     end
     html.html_safe
   end
