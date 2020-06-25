569865114071b943c2d69c68fdee37c11eb6e8bf
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-25 15:46:58 +0600

    Fix rubocop errors

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,14 +4,16 @@ module UserHelper
 
     if Friendship.occurence_in_friendship(id, current_user.id).count.positive?
       friend = Friendship.occurence_in_friendship(id, current_user.id).pluck(:id)
-      html += link_to('Accept Friendship', friendship_path(id: friend[0], invitor_id: current_user.id, invitee_id: id, status: true), method: :put)
+      accept_params = { id: friend[0], invitor_id: current_user.id, invitee_id: id, status: true }
+      html += link_to('Accept Friendship', friendship_path(accept_params), method: :put)
       html += ' '
-      html += link_to('Reject friendship', friendship_path(id: friend[0], invitor_id: current_user.id, invitee_id: id, status: true), method: :delete)
+      reject_params = { id: friend[0], invitor_id: current_user.id, invitee_id: id, status: true }
+      html += link_to('Reject friendship', friendship_path(reject_params), method: :delete)
     elsif Friendship.occurence_in_friendship2(id, current_user.id).count == 1
       html += 'Already Friends'
     elsif Friendship.occurence_in_friendship2(current_user.id, id).count == 1
       html += 'Already Friends'
-    elsif Friendship.occurence_in_friendship(current_user.id, id).count > 0
+    elsif Friendship.occurence_in_friendship(current_user.id, id).count.positive?
       html += 'Awaiting acceptance from user'
     else
       html += link_to('Invite', friendships_path(invitor_id: current_user.id, invitee_id: id), method: :post)
