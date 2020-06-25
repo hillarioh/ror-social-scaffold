35a8fb53efef52cab344199bbc3523178c55df8f
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-25 20:58:47 +0600

    Change model to user and friend

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,10 +1,10 @@
 class Friendship < ApplicationRecord
-  belongs_to :invitor, class_name: 'User'
-  belongs_to :invitee, class_name: 'User'
+  belongs_to :user
+  belongs_to :friend, :class_name => "User"
 
-  validates_presence_of :invitor
-  validates_presence_of :invitee
+  validates_presence_of :user
+  validates_presence_of :friend
 
-  scope :occurence_in_friendship, ->(r, e) { where(invitor: r, invitee: e, status: false) }
-  scope :occurence_in_friendship2, ->(r, e) { where(invitor: r, invitee: e, status: true) }
+  scope :occurence_in_friendship, ->(r, e) { where(user: r, friend: e, status: false) }
+  scope :occurence_in_friendship2, ->(r, e) { where(user: r, friend: e, status: true) }
 end
