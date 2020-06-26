39150957c68e4c3db65f111a22ade544f514d41e
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-26 20:49:48 +0600

    Add test cases and fix rubocop errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,17 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   context 'Check for validation of user input' do
-#     it 'is valid with valid attributes' do
-#       expect(User.create).to_not be_valid
-#     end
+RSpec.describe User, type: :model do
+  context 'Check for validation of user input' do
+    it 'is valid with valid attributes' do
+      expect(User.create).to_not be_valid
+    end
 
-#     it 'is not valid without a username' do
-#       expect(User.new(username: nil)).to_not be_valid
-#     end
-#   end
+    it 'is not valid without a username' do
+      expect(User.new(name: nil)).to_not be_valid
+    end
 
-#   context 'ActiveRecord associations' do
-#     it 'has many events' do
-#       expect(User.reflect_on_association(:events).macro).to be: has_many
-#     end
-#   end
-# end
+    it 'is not valid without an email' do
+      expect(User.new(email: nil)).to_not be_valid
+    end
+  end
+end
