629a827c4a0378595ec0de860a1b10ab11ee3cb6
hillarioh <okerioh@gmail.com>
2020-06-26 11:09:47 +0300

    add scope, edit timeline_posts method

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -7,4 +7,6 @@ class Friendship < ApplicationRecord
 
   scope :occurence_in_friendship, ->(r, e) { where(user: r, friend: e, status: false) }
   scope :occurence_in_friendship2, ->(r, e) { where(user: r, friend: e, status: true) }
+  scope :posting_friend, ->(c){where(friend: c, status: true)}
+  scope :posting_user, ->(c){where(user: c, status: true)}
 end
